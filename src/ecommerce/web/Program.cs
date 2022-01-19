16ff9bd18c832a1d21bbb9ec2a79952d1a9ef7c6
Maciej Gos <maciej@maciejgos.com>
2022-01-19 07:30:12 +0100

    #8 E-Commerce UI (#30)

--- src/ecommerce/web/Program.cs
+++ src/ecommerce/web/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:8080") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://advcore-apim.azure-api.net") });
 
 await builder.Build().RunAsync();
